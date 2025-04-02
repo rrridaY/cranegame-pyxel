3486cf6896c03cea79fc4ed81ca99d6f2364a233
kohak_desk <koha.yaspa@icloud.com>
2025-04-02 20:23:21 +0900

    範囲判定関数修正

--- script/Classes/Clickpos.py
+++ script/Classes/Clickpos.py
@@ -17,6 +17,9 @@ class ClickPos(Vector2):
     def is_clicked(self,click_pos:Vector2):
         return self.pos.x <= click_pos.x <= self.pos.x + 1 and self.pos.y <= click_pos.y <= self.pos.y + 1
     
-    def is_in_texture_range(self,target:Vector2):
+    def is_in_range(self,target:Vector2):
+        """
+        クリック位置がtargetの範囲内かどうかを判定する関数\n
+        target: 判定対象の座標 (Vector2,現在の位置)\n"""
         print("is_in_texture_range")
         return target.pos.x <= self.pos.x <= target.pos.x + target.texture.size.x and target.pos.y <= self.pos.y <= target.pos.y + target.texture.size.y
